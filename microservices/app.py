39cf64bec8aa3f97703785404ef3dfe6c8c9509e
aldemarf <aldemar.poli@gmail.com>
2019-11-03 18:03:10 -0300

    Implemented events_generator routes | Minor fixes | Error 403 handler

--- microservices/app.py
+++ microservices/app.py
@@ -1,20 +1,21 @@
-from flask import Flask, make_response, jsonify, request
+from flask import Flask, make_response, jsonify, request, abort
+from stations.event_generator import event_generator, stop_generator
 from thingsboard.api import *
 from distance.haversine import *
 from hi.heat_index import *
 import logging
-
+import stations
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)-10s %(levelname)-6s %(message)s')
 
 app = Flask(__name__)
 
 #################################################
-################## 5_NEAREST ####################
+################## 5-NEAREST ####################
 #################################################
 
 
-help_5near = """
+help_5_nearest = """
         <strong>arg 1</strong>: t;</br>
         <strong>description</strong>: The instant temperature measured at the station;</br>
         </br>
@@ -27,17 +28,17 @@ help_5near = """
         <strong>Examples:</strong></br>
         http://host/api/5near?lat=-34.06218487&lon=-8.41657131
         </br> """
-        # ()-> JSON{distance: 16.345000000000002} (km)
+# ()-> JSON{distance: 16.345000000000002} (km)
 
 
 @app.route('/api/5near/status')
-def status_5nearnear():
+def status_5nearest():
     return 'Server : Running'
 
 
 @app.route('/api/5near/help')
-def show_help_5near():
-    return help_5near
+def show_help_5_nearest():
+    return help_5_nearest
 
 
 @app.route('/api/5near/', methods=['GET'])
@@ -59,11 +60,11 @@ def get_5_nearest():
 
 
 #################################################
-################## HAVERSINE ####################
+################# HEAT INDEX ####################
 #################################################
 
 
-help_haversine = """
+help_heat_index = """
         <strong>arg 1</strong>: t;</br>
         <strong>description</strong>: The instant temperature measured at the station;</br>
         </br>
@@ -90,20 +91,91 @@ def status_hi():
 
 @app.route('/api/hi/help')
 def show_help_hi():
-    return help_haversine
+    return help_heat_index
 
 
 @app.route('/api/hi/', methods=['GET'])
-def calc_hi_float():
+def calc_hi():
     try:
-        temp = float(request.args['temp'])
-        rh = float(request.args['rh'])
+        data = request.args
+        temp = float(data['temp'])
+        rh = float(data['rh'])
         hi = heat_index(temp, rh)
         return jsonify({'heat_index': hi})
 
     except Exception as e:
         logging.error(f'INVALID ENTRY: {e}')
         return jsonify({'heat_index': None})
+
+
+#################################################
+############### EVENT GENERATOR #################
+#################################################
+
+threads = set()
+running = False
+help_event_generator = """
+        <strong>arg 1</strong>: proc;</br>
+        <strong>description</strong>: Type of processing SINGLE or MULTI;</br>
+        </br>
+        <strong>arg 2</strong>: inter;</br>
+        <strong>description</strong>: The interval between stations readings;</br>
+        </br>
+        <strong>return</strong>: Nothing;</br>
+        </br>
+        </br>
+        <strong>Example:</strong></br>
+        http://host/api/event_generator/start?proc=multi&inter=10</br>
+        </br>
+        http://host/api/event_generator/start?proc=single&inter=1.5</br>
+"""
+
+
+@app.route('/api/event_generator')
+def event_gen_help():
+    return help_event_generator
+
+
+@app.route('/api/event_generator/start', methods=['GET'])
+def start_generate_events():
+    global running, threads
+
+    if running:
+        return abort(405)
+
+    try:
+        processing = request.args['proc']
+    except KeyError as error:
+        processing = 'multi'
+        logging.warning(error)
+
+    try:
+        interval = float(request.args['inter'])
+    except KeyError as error:
+        interval = 2
+        logging.warning(error)
+
+    logging.info('Starting events generation')
+    if processing == 'multi' or processing == 'single':
+        running = event_generator(processing=processing, publish_interval=interval)
+    else:
+        return f'<h2>Events generation : Failed!<h2>' \
+               f'<h3>Parameters:</h3>' \
+               f'Method: {processing} threaded</br>' \
+               f'Interval: {interval} seconds'
+
+    return f'<h2>Events generation : OK!<h2>' \
+           f'<h3>Parameters:</h3>' \
+           f'Method: {processing} threaded</br>' \
+           f'Interval: {interval} seconds'
+
+
+@app.route('/api/event_generator/stop', methods=['GET'])
+def stop_generate_events():
+    global running
+    logging.info('Stopping events generation')
+    running = stop_generator()
+    return '<h3>Stopped events generation</h3>'
 
 
 #################################################
@@ -115,6 +187,12 @@ def calc_hi_float():
 def bad_request(error):
     logging.error(error)
     return make_response(jsonify({f'{error.code}': 'Bad request'}), 400)
+
+
+@app.errorhandler(403)
+def not_found(error):
+    logging.error(error)
+    return make_response(jsonify({f'{error.code}': 'Not found'}), 403)
 
 
 @app.errorhandler(404)
