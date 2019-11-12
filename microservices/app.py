25cd70ebe99a7f3bd1f5de60cf0ba229b634d88b
aldemarf <aldemar.poli@gmail.com>
2019-11-11 22:00:10 -0300

    2 New routes: api/bridge/stop + api/event_generator/start/{station} | Minor fixes

--- microservices/app.py
+++ microservices/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, make_response, jsonify, request, abort
-from stations.event_generator import event_generator, stop_generator
+from stations.event_generator import process_all_stations, process_single_station, stop_generator
 from stations import bridge_kafka_tb
 from thingsboard.api import *
 from distance.haversine import *
@@ -50,7 +50,7 @@ def get_5_nearest():
         lat = float(data['lat'])
         lon = float(data['lon'])
 
-        distance = dist_haversine(lat, lon, lat2=-8.0453602, lon2=-34.941812)
+        distance = haversine(lat, lon, lat2=-8.0453602, lon2=-34.941812)
 
         # TODO: CONTINUE IMPLEMENTATION
         # stations = []
@@ -146,48 +146,75 @@ def start_generate_events():
         return abort(405)
 
     try:
-        processing = request.args['proc']
-    except KeyError as error:
-        processing = 'multi'
-        logging.warning(error)
-
-    try:
         interval = float(request.args['inter'])
     except KeyError as error:
         interval = 2
         logging.warning(error)
 
     logging.info('Starting events generation')
-    if processing == 'multi' or processing == 'single':
-        running = event_generator(processing=processing, publish_interval=interval)
-    else:
-        return f'<h2>Events generation : Failed!<h2>' \
-               f'<h3>Parameters:</h3>' \
-               f'Method: {processing} threaded</br>' \
-               f'Interval: {interval} seconds'
+    running = process_all_stations(publish_interval=interval)
 
-    return f'<h2>Events generation : OK!<h2>' \
-           f'<h3>Parameters:</h3>' \
-           f'Method: {processing} threaded</br>' \
-           f'Interval: {interval} seconds'
+    if running:
+        return f'<h2>Events generation : OK!<h2>' \
+               f'Processing all stations multithreaded</br>' \
+               f'Interval: {interval} seconds'
+    else:
+        return f'<h2>Events generation : Failed!<h2>'
+
+
+@app.route('/api/event_generator/start/<string:station>', methods=['GET'])
+def start_generate_events_single_station(station):
+    global threads
+
+    stations = {thread.name for thread in threads}
+
+    if running and station in stations:
+        return abort(405)
+
+    try:
+        interval = float(request.args['interval'])
+    except KeyError as error:
+        interval = 2
+        logging.warning(error)
+
+    logging.info(f'Starting events generation from station {station}')
+    response = process_single_station(station=station, publish_interval=interval)
+
+    if response:
+        return f'<h2>Events generation : OK!<h2>' \
+               f'Processing station {station} single-threaded</br>' \
+               f'Interval: {interval} seconds'
+    else:
+        return f'<h2>Events generation : Failed!<h2>'
 
 
 @app.route('/api/event_generator/stop', methods=['GET'])
 def stop_generate_events():
+    global running
+
     logging.info('Stopping events generation')
     status = stop_generator()
+    running = False
     return f'<h3>Stopped events generation</h3>' \
            f'{status}'
 
 
 #################################################
-############### KAFKA TB BRIDGE #################
+############### KAFKA-TB BRIDGE #################
 #################################################
 
 
 @app.route('/api/bridge/start', methods=['GET'])
 def start_bridge():
     bridge_kafka_tb.start_bridge()
+
+
+@app.route('/api/bridge/stop', methods=['GET'])
+def stop_MQTT_bridge():
+    logging.info('Stopping events generation')
+    status = bridge_kafka_tb.stop_bridge()
+    return f'<h3>Stopped events generation</h3>' \
+           f'{status}'
 
 
 
@@ -205,7 +232,7 @@ def bad_request(error):
 @app.errorhandler(403)
 def not_found(error):
     logging.error(error)
-    return make_response(jsonify({f'{error.code}': 'Not found'}), 403)
+    return make_response(jsonify({f'{error.code}': 'Forbidden'}), 403)
 
 
 @app.errorhandler(404)
