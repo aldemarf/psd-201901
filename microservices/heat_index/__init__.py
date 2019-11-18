f99bec133ad057c50dbb9d1998c0753ddb38f456
aldemarf <aldemar.poli@gmail.com>
2019-11-18 14:00:32 -0300

    Heat Index funcionando com falhas no streaming (consumer) -- Precisa de mais testes

--- microservices/heat_index/__init__.py
+++ microservices/heat_index/__init__.py
@@ -1,8 +1,9 @@
 from thingsboard.api import *
 from kafka import KafkaConsumer
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StringType, FloatType, StructType, StructField
-from pyspark.sql.functions import udf, from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType, ArrayType, \
+    DoubleType
+from pyspark.sql.functions import udf, from_json, col, unix_timestamp
 from math import sqrt
 
 # spark.sparkContext.setLogLevel("ERROR")
@@ -14,6 +15,13 @@ def ctf(temp):
 
 @udf(returnType=FloatType())
 def heat_index(t=89.6, rh=35):
+
+    if not isinstance(t, float):
+        t = float(t)
+
+    if not isinstance(rh, float):
+        rh = float(rh)
+
     hi = (1.1 * t) + (0.047 * rh) - 10.3
 
     if hi < 80:
@@ -60,44 +68,65 @@ def calc_stations_hi():
 
 host = 'localhost'
 port = '9092'
-subscribe_type = 'subscribe'
+subscribe_type = 'subscribePattern'
 consumer = KafkaConsumer(group_id='topics', bootstrap_servers=[f'{host}:{port}'])
 topics_list = [topic for topic in consumer.topics() if topic[:8] == 'estacoes']
 topics = ','.join(topics_list)
-# topics = 'estacoes.Recife.A301'
-consumer.subscribe(topics=topics_list)
-print(next(consumer))
 consumer.close()
-startingOffset = 'earliest'
+topics_pattern = 'estacoes.*'
+startingOffset = 'latest'
 
 spark = SparkSession.builder\
     .appName('HeatIndex')\
     .getOrCreate()
 
-df = spark.readStream.format('kafka')\
+df = spark.readStream\
+    .format('kafka')\
     .option('kafka.bootstrap.servers', f'{host}:{port}')\
-    .option(subscribe_type, topics)\
+    .option(subscribe_type, topics_pattern)\
     .option('startingOffsets', startingOffset)\
-    .load()
-df.printSchema()
+    .load()\
+    .selectExpr('CAST(value as STRING)')
 
-raw_df = df.selectExpr('CAST(value AS STRING)')
-raw_df.printSchema()
+schema = StructType()\
+    .add('timestamp', StringType())\
+    .add('stationCode', StringType())\
+    .add('stationName', StringType())\
+    .add('latitude', StringType())\
+    .add('longitude', StringType())\
+    .add('umid_max', StringType())\
+    .add('umid_min', StringType())\
+    .add('temp_max', StringType())\
+    .add('pressao', StringType())\
+    .add('pressao_min', StringType())\
+    .add('pto_orvalho_inst', StringType())\
+    .add('pto_orvalho_max', StringType())\
+    .add('radiacao', StringType())\
+    .add('temp_min', StringType())\
+    .add('pressao_max', StringType())\
+    .add('pto_orvalho_min', StringType())\
+    .add('temp_inst', StringType())\
+    .add('umid_inst', StringType())\
+    .add('precipitacao', StringType())
 
-schema = StructType([
-    StructField('stationCode', StringType()),
-    StructField('temp_inst', FloatType()),
-    StructField('umid_inst', FloatType())
-])
+sta_df = df.select(from_json(col('value').cast('string'), schema=schema).alias('sta'))\
+    .select('sta.*')\
+    .select('stationName', 'stationCode', 'latitude', 'longitude', 'temp_inst', 'umid_inst')
 
+sta_df.printSchema()
 
+consoleOutput = sta_df.writeStream\
+    .outputMode("append")\
+    .format("console")\
+    .start()
+consoleOutput.awaitTermination(timeout=10)
 
-hi_df = raw_df.select(from_json(col("value").cast("string"), schema).alias("data"))  # .select("data.*")
-
-query = hi_df.writeStream.format("console").outputMode("append").start().awaitTermination()
-
-# raw_df.createOrReplaceTempView('stations')
+heat_index_df = sta_df.withColumn('Heat_Index', heat_index('temp_inst', 'umid_inst'))
 #
-# hi = spark.sql('SELECT * FROM stations')
+consoleOutput = heat_index_df.writeStream\
+    .outputMode("append")\
+    .format("console")\
+    .start()
+consoleOutput.awaitTermination(timeout=10)
 
 print('')
