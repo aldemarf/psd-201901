15c975e2bd06096e7ecc923c6c0066c886128248
aldemarf <aldemar.poli@gmail.com>
2019-11-19 07:37:36 -0300

    1 new function: calc_stations_hi()

--- microservices/heat_index/__init__.py
+++ microservices/heat_index/__init__.py
@@ -1,50 +1,59 @@
+from conf import KAFKA_HOST, KAFKA_PORT
+from heat_index.publisher import RowPublisher
+
 from thingsboard.api import *
-from kafka import KafkaConsumer
+
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, FloatType, TimestampType, ArrayType, \
-    DoubleType
-from pyspark.sql.functions import udf, from_json, col, unix_timestamp
+from pyspark.sql.types import StructType, StringType, FloatType
+from pyspark.sql.functions import udf, from_json, col
+
 from math import sqrt
 
-# spark.sparkContext.setLogLevel("ERROR")
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(name)-10s %(levelname)-6s %(message)s')
+
+HOST = f'{KAFKA_HOST}:{KAFKA_PORT}'
 
 
-def ctf(temp):
-    return temp * 9 / 5 + 32
+def ctf(temp):  # Celsius to Fahrenheit
+    return (temp * 9 / 5) + 32
 
 
-@udf(returnType=FloatType())
-def heat_index(t=89.6, rh=35):
+def ftc(temp):  # Fahrenheit to Celsius
+    return (temp - 32) * 5 / 9
 
-    if not isinstance(t, float):
-        t = float(t)
 
-    if not isinstance(rh, float):
-        rh = float(rh)
+@udf(returnType=FloatType())  # User Defined Function (Spark annotation)
+def heat_index(t, rh):
 
-    hi = (1.1 * t) + (0.047 * rh) - 10.3
+    try:
+        temp = ctf(float(t.strip()))
+        hum = float(rh.strip())
+
+    except Exception:
+        return None
+
+    hi = (1.1 * temp) + (0.047 * hum) - 10.3
 
     if hi < 80:
-        return hi
-
+        return ftc(hi)
     else:
-        hi = -42.379 + (2.04901523 * t + 10.14333127 * rh) \
-             - (0.22475541 * t * rh) - (0.00683783 * t**2) \
-             - (0.05481717 * rh**2) + (0.00122874 * t**2 * rh) \
-             + (0.00085282 * t * rh**2) - (0.00000199 * t**2 * rh**2)
+        hi = -42.379 + (2.04901523 * temp + 10.14333127 * hum) \
+             - (0.22475541 * temp * hum) - (0.00683783 * temp ** 2) \
+             - (0.05481717 * hum**2) + (0.00122874 * temp ** 2 * hum) \
+             + (0.00085282 * temp * hum ** 2) - (0.00000199 * temp ** 2 * hum ** 2)
 
         # 80 <= T <= 112 && RH <= 13%
-        if (rh < 13) and (80 <= t <= 112):
-            adjustment_subtraction = ((13 - rh) / 4) * sqrt((17 - abs(t - 95) / 17))
-            return hi - adjustment_subtraction
+        if (hum < 13) and (80 <= temp <= 112):
+            adjustment_subtraction = ((13 - hum) / 4) * sqrt((17 - abs(temp - 95) / 17))
+            return ftc(hi - adjustment_subtraction)
 
         # 80 <= T <= 87 && RH > 85%
-        elif (rh > 85) and (80 <= t <= 87):
-            adjustment_addition = ((rh - 85) / 10) * ((87 - t) / 5)
-            return hi + adjustment_addition
+        elif (hum > 85) and (80 <= temp <= 87):
+            adjustment_addition = ((hum - 85) / 10) * ((87 - temp) / 5)
+            return ftc(hi + adjustment_addition)
 
         else:
-            return hi
+            return ftc(hi)
 
 
 def get_stations_hi(station_type='ESTAÇÃO METEOROLÓGICA'):
@@ -61,72 +70,71 @@ def get_stations_hi(station_type='ESTAÇÃO METEOROLÓGICA'):
     return stations_heat_index
 
 
-def calc_stations_hi():
+def start_hi_calc(host=KAFKA_HOST, port=KAFKA_PORT):
+    try:
+        # consumer = KafkaConsumer(group_id='topics', bootstrap_servers=[f'{host}:{port}'])
+        # topics_list = [topic for topic in consumer.topics() if topic[:8] == 'estacoes']
+        # topics = ','.join(topics_list)
+        # consumer.close()
+        subscribe_type = 'subscribePattern'
+        topics_pattern = 'estacoes.*'
+        startingOffset = 'latest'
 
-    return NotImplemented
+        spark = SparkSession.builder\
+            .appName('HeatIndex')\
+            .getOrCreate()
 
+        df = spark.readStream \
+            .format('kafka') \
+            .option('kafka.bootstrap.servers', f'{host}:{port}') \
+            .option(subscribe_type, topics_pattern) \
+            .option('startingOffsets', startingOffset) \
+            .option('maxOffsetsPerTrigger', '1') \
+            .load() \
+            .selectExpr('CAST(value AS STRING)')
 
-host = 'localhost'
-port = '9092'
-subscribe_type = 'subscribePattern'
-consumer = KafkaConsumer(group_id='topics', bootstrap_servers=[f'{host}:{port}'])
-topics_list = [topic for topic in consumer.topics() if topic[:8] == 'estacoes']
-topics = ','.join(topics_list)
-consumer.close()
-topics_pattern = 'estacoes.*'
-startingOffset = 'latest'
+        schema = StructType() \
+            .add('timestamp', StringType()) \
+            .add('stationCode', StringType()) \
+            .add('stationName', StringType()) \
+            .add('latitude', StringType()) \
+            .add('longitude', StringType()) \
+            .add('umid_max', StringType()) \
+            .add('umid_min', StringType()) \
+            .add('temp_max', StringType()) \
+            .add('pressao', StringType()) \
+            .add('pressao_min', StringType()) \
+            .add('pto_orvalho_inst', StringType()) \
+            .add('pto_orvalho_max', StringType()) \
+            .add('radiacao', StringType()) \
+            .add('temp_min', StringType()) \
+            .add('pressao_max', StringType()) \
+            .add('pto_orvalho_min', StringType()) \
+            .add('temp_inst', StringType()) \
+            .add('umid_inst', StringType()) \
+            .add('precipitacao', StringType())
 
-spark = SparkSession.builder\
-    .appName('HeatIndex')\
-    .getOrCreate()
+        sta_df = df.select(from_json(col('value').cast('string'), schema=schema).alias('sta'))\
+            .select('sta.stationName',
+                    'sta.stationCode',
+                    'sta.temp_inst',
+                    'sta.umid_inst')
 
-df = spark.readStream\
-    .format('kafka')\
-    .option('kafka.bootstrap.servers', f'{host}:{port}')\
-    .option(subscribe_type, topics_pattern)\
-    .option('startingOffsets', startingOffset)\
-    .load()\
-    .selectExpr('CAST(value as STRING)')
+        heat_index_df = sta_df.withColumn('heat_index', heat_index('temp_inst', 'umid_inst'))
 
-schema = StructType()\
-    .add('timestamp', StringType())\
-    .add('stationCode', StringType())\
-    .add('stationName', StringType())\
-    .add('latitude', StringType())\
-    .add('longitude', StringType())\
-    .add('umid_max', StringType())\
-    .add('umid_min', StringType())\
-    .add('temp_max', StringType())\
-    .add('pressao', StringType())\
-    .add('pressao_min', StringType())\
-    .add('pto_orvalho_inst', StringType())\
-    .add('pto_orvalho_max', StringType())\
-    .add('radiacao', StringType())\
-    .add('temp_min', StringType())\
-    .add('pressao_max', StringType())\
-    .add('pto_orvalho_min', StringType())\
-    .add('temp_inst', StringType())\
-    .add('umid_inst', StringType())\
-    .add('precipitacao', StringType())
+        # consoleOutput = heat_index_df.writeStream\
+        #     .outputMode("append")\
+        #     .format("console")\
+        #     .start()
+        # consoleOutput.awaitTermination(timeout=10)
 
-sta_df = df.select(from_json(col('value').cast('string'), schema=schema).alias('sta'))\
-    .select('sta.*')\
-    .select('stationName', 'stationCode', 'latitude', 'longitude', 'temp_inst', 'umid_inst')
+        ds = heat_index_df\
+            .writeStream \
+            .foreach(RowPublisher()) \
+            .start()
 
-sta_df.printSchema()
+        return True
 
-consoleOutput = sta_df.writeStream\
-    .outputMode("append")\
-    .format("console")\
-    .start()
-consoleOutput.awaitTermination(timeout=10)
-
-heat_index_df = sta_df.withColumn('Heat_Index', heat_index('temp_inst', 'umid_inst'))
-#
-consoleOutput = heat_index_df.writeStream\
-    .outputMode("append")\
-    .format("console")\
-    .start()
-consoleOutput.awaitTermination(timeout=10)
-
-print('')
+    except Exception as error:
+        logging.error(error)
+        return False
